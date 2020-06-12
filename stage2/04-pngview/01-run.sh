135cc55f8beed4a5c632969a0eb751e07a8a82b8
Yurii Danilovskyi <yurii@renetec.io>
2020-06-12 22:12:47 +0300

    Add splash images for different resolutions.

--- stage2/04-pngview/01-run.sh
+++ stage2/04-pngview/01-run.sh
@@ -3,8 +3,10 @@
 install -v -m 755 files/pngview                  "${ROOTFS_DIR}/usr/bin/"
 install -v -m 777 files/libraspidmx.so.1         "${ROOTFS_DIR}/usr/lib/"
 install -v -m 644 files/splashscreen.service     "${ROOTFS_DIR}/etc/systemd/system/"
-install -v -m 644 files/splash.png               "${ROOTFS_DIR}/opt/"
+install -d                                       "${ROOTFS_DIR}/usr/share/splash_images/"
+install -v -m 644 files/splash_images/*          "${ROOTFS_DIR}/usr/share/splash_images/"
 
 on_chroot << EOF
 systemctl enable splashscreen.service
+ln -s /usr/share/splash_images/splash_normal_420.png /opt/splash.png
 EOF
