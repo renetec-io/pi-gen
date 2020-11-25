ae90c574a1f5404c58aa01234925efd3b2dae481
Yurii Danilovskyi <yurii@renetec.io>
2020-11-25 07:12:26 +0200

    stage3-pyxis-module:01-i2c-tweaks: Enable autoload for i2c driver.

--- stage3-pyxis-module/01-i2c-tweaks/00-run.sh
+++ stage3-pyxis-module/01-i2c-tweaks/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 CONFIG=${ROOTFS_DIR}/boot/config.txt
+LOADMOD=${ROOTFS_DIR}/etc/modules
 
 # Compile and install device tree overlay blob
 dtc -I dts -O dtb -o i2c0_44_45.dtbo files/i2c0_44_45.dts
@@ -10,3 +11,10 @@ echo -e "
 # Enable I2C0 on pins 44, 45
 dtoverlay=i2c0_44_45
 dtparam=i2c_vc=on" | tee -a $CONFIG > /dev/null
+
+if [ -e $LOADMOD ] && grep -q "^i2c-dev$" $LOADMOD; then
+  echo "i2c-dev already added"
+else
+  echo "i2c-dev" >> $LOADMOD
+fi
+
