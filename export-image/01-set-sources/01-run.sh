e6bbc78d4ac0661cf26a0938d8fff88861e9304d
shtig <igor@renetec.io>
2021-08-01 22:15:26 -0700

    Change package repository to pyxis.dev

--- export-image/01-set-sources/01-run.sh
+++ export-image/01-set-sources/01-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # Done with build-time package installations, remove staging repository
-sed -i '/pyxian-staging/d' "${ROOTFS_DIR}/etc/apt/sources.list.d/pyxian.list"
+sed -i '/staging/d' "${ROOTFS_DIR}/etc/apt/sources.list.d/pyxian.list"
 
 on_chroot << EOF
 apt-get update
